5e5f97a67be1a2b7cb44d330aa20d9186f12c606
hask777 <haskellisp@gmail.com>
2020-09-16 17:46:09 +0300

    init

---
+++ app/Http/Controllers/AreasController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class AreasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $req = 'https://api.sportsdata.io/v3/soccer/scores/json/Areas';
+        $header = 'f7b0c7419d204f299f59e646b45ca563';
+
+        $areas = Http::withHeaders(['Ocp-Apim-Subscription-Key' => $header])->get($req)->json();
+
+        // dd($areas);
+
+        return view('areas', ['areas' => $areas]); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/areas.blade.php
@@ -0,0 +1,5 @@
+@foreach($areas as $area)
+
+	<a href="#">{{$area['Name']}}</a>	
+
+@endforeach
